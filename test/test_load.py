c5c137b1d97be724dabdfd64c4a812865d321a37
Ridwan Clouds <137292272+ridwanxyzcloud@users.noreply.github.com>
2024-07-15 21:28:54 +0100

    update to the etl pipeline and modular codes

--- test/test_load.py
+++ test/test_load.py
@@ -1,12 +1,14 @@
 import os
 import sys
+import pandas as pd
+
 
 # Adding the parent directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from etl_modules.load import load_to_snowflake
+from etl_modules.load import load_to_postgres
 
 
 output_path = './processed_data/transformed_data.csv'
 table_name = 'goalbet_data'
-load_to_snowflake(output_path,table_name)
+load_to_postgres(output_path,table_name)
